91f837ca7c05dd762b8c59fed0cc187aae9a9dba
Matthew Nelson <developer@matthewnelson.io>
2022-01-14 10:20:19 -0500

    remove Platform class and ref (#9)

--- build-configuration/src/commonMain/kotlin/io/matthewnelson/component/build/configuration/BuildConfiguration.kt
+++ build-configuration/src/commonMain/kotlin/io/matthewnelson/component/build/configuration/BuildConfiguration.kt
@@ -15,6 +15,8 @@
  **/
 package io.matthewnelson.component.build.configuration
 
+import kotlin.jvm.JvmOverloads
+
 /**
  * Enable the passing of BuildConfig information across modules
  *
@@ -23,12 +25,11 @@ package io.matthewnelson.component.build.configuration
  *  - [versionName] is empty
  *  - [versionCode] is less than or equal to 0
  * */
-data class BuildConfiguration(
+data class BuildConfiguration @JvmOverloads constructor(
     val applicationId: String,
     val versionName: String,
     val versionCode: Int,
     val isDebug: Boolean,
-    val platform: Platform,
     val variant: String = if (isDebug) "debug" else "release"
 ) {
 
@@ -36,24 +37,6 @@ data class BuildConfiguration(
         check(applicationId.isNotEmpty()) { "applicationId cannot be empty" }
         check(versionName.isNotEmpty()) { "versionName cannot be empty" }
         check(versionCode > 0) { "versionCode must be greater than 0" }
-    }
-
-    sealed class Platform {
-
-        override fun equals(other: Any?): Boolean {
-            return other != null && other is Platform && other.toString() == toString()
-        }
-
-        override fun hashCode(): Int {
-            return 17 * 31 + toString().hashCode()
-        }
-
-        class Android: Platform() { override fun toString(): String = "Platform.Android" }
-        class Jvm: Platform() { override fun toString(): String = "Platform.Jvm" }
-        class Js: Platform() { override fun toString(): String = "Platform.Js" }
-        class Darwin: Platform() { override fun toString(): String = "Platform.Darwin" }
-        class Linux: Platform() { override fun toString(): String = "Platform.Linux" }
-        class Mingw: Platform() { override fun toString(): String = "Platform.Mingw" }
     }
 
 }
