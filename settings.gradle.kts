ab8de63aa097c39cf6be1c72b789e252a007a13c
Matthew Nelson <developer@matthewnelson.io>
2022-05-10 20:18:24 -0400

    Fix check-publication functionality

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,10 @@ rootProject.name = "component-build-configuration"
 includeBuild("kotlin-components/includeBuild/dependencies")
 includeBuild("kotlin-components/includeBuild/kmp")
 
-include(":build-configuration")
-
 @Suppress("PrivatePropertyName")
 private val CHECK_PUBLICATION: String? by settings
 if (CHECK_PUBLICATION != null) {
     include(":tools:check-publication")
+} else {
+    include(":build-configuration")
 }
